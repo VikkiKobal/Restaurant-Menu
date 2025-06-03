e32d16651fcb9ee451e169a7b9e8ec58061f019a
Viktoriia Kobal <kobal.viktoriia@student.uzhnu.edu.ua>
2025-06-03 18:31:18 +0300

    favicon is added

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
     transpileDependencies: true,
-})
-
-module.exports = {
     devServer: {
         proxy: {
             '/api': {
                 target: 'http://localhost:3000',
                 changeOrigin: true,
-            }
-        }
-    }
-}
+            },
+        },
+    },
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            title: 'Cibo Gustoso',
+        },
+    },
+})
