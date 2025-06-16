2e9d6f4dc82dd887bf1ee72010bbcddb3d47930f
Viktoriia Kobal <kobal.viktoriia@student.uzhnu.edu.ua>
2025-06-16 19:42:50 +0300

    UI improvement in filter section

--- src/components/CategoryFilter.vue
+++ src/components/CategoryFilter.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="category-filter">
         <button
-            v-for="category in filteredCategories"
+            v-for="(category, index) in filteredCategories"
             :key="category.id"
-            @click="handleClick(category.id, $event)"
+            @click="handleClick(category.id, index)"
             :class="['filter-button', { active: selectedCategoryId === category.id }]"
+            ref="filterButtons"
         >
             {{ category.name }}
         </button>
     </div>
 </template>
-
 
 <script setup>
 import { ref, onMounted, computed } from 'vue'
@@ -19,6 +19,7 @@ import axios from 'axios'
 const categories = ref([])
 const error = ref(null)
 const selectedCategoryId = ref(null)
+const filterButtons = ref([])
 
 const filteredCategories = computed(() => categories.value.filter((category) => category.name !== 'Specialities'))
 
@@ -26,7 +27,6 @@ onMounted(async () => {
     try {
         const response = await axios.get(`${process.env.VUE_APP_API_URL}/api/categories`)
         categories.value = response.data
-        console.log('Fetched categories:', categories.value)
     } catch (err) {
         error.value = err
         console.error('Помилка при завантаженні категорій:', err)
@@ -34,17 +34,20 @@ onMounted(async () => {
 })
 
 const emit = defineEmits(['update:categoryId'])
-function handleClick(categoryId, event) {
+
+function handleClick(categoryId, index) {
     if (selectedCategoryId.value === categoryId) {
         selectedCategoryId.value = null
         emit('update:categoryId', null)
-        event.target.blur()
+
+        filterButtons.value[index]?.blur()
     } else {
         selectedCategoryId.value = categoryId
         emit('update:categoryId', categoryId)
     }
 }
 </script>
+
 
 <style lang="scss" scoped>
 .category-filter {
